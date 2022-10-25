404fbee2a0a6493973418e2460a5201b321937fe
Brito <dibrito@teksystems.com>
2022-10-25 13:31:28 -0700

    changed setText to (data.slip)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-
 import "./App.css";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import dice from "./images/icon-dice.svg";
 import dividerMobile from "./images/pattern-divider-mobile.svg";
 import dividerDesktop from "./images/pattern-divider-desktop.svg";
 
 function App() {
-  function fetchAdvice(){
-    fetch("https://api.adviceslip.com/advice")
-    .then(response => response.json())
-    .then(data => setAdvice(data));
+  const [text, setText] = useState([]);
+
+  const fetchAdvice = async () => {
+    const res = await fetch("https://api.adviceslip.com/advice")
+    const data = await res.json();
+
+    setText(data.slip)
   }
-  
-  const [advice, setAdvice] = useState([]);
+
   useEffect(() => {
     fetchAdvice()
-  }, []);
-
+  }, [])
 
   return (
     <div className="container">
-      <h1>Advice #{advice.slip.id}</h1>
-      <p>{advice.slip.advice}</p>
+      <h1>Advice #{text.id}</h1>
+      <p>{text.advice}</p>
 
       <picture>
         <source media="(min-width: 768px)" srcSet={dividerDesktop} />
